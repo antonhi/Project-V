be5474d045ccb0bc1738369c698bb02cf64f482b
antonhorvath <aih180000@utdallas.edu>
2022-03-18 17:50:35 -0500

    Firebase Core Added

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,14 @@ import 'package:projectv/controllers/routes/router.dart';
 import 'package:projectv/services/authentication/authentication.dart';
 import 'package:projectv/services/database/database.dart';
 import 'package:projectv/services/storage/storage.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -19,15 +25,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: BlocProvider<Controller>(
